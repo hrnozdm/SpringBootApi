6f58aab144b1f1242242f05dbf489bb73157a48f
Harun Özdemir <80276787+hrnozdm@users.noreply.github.com>
2024-09-20 01:53:58 +0300

    Create Project and İnital Db

---
+++ dream-shops/src/main/java/com/dailycodework/dream_shops/Model/Product.java
@@ -0,0 +1,31 @@
+package com.dailycodework.dream_shops.Model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String brand;
+    private BigDecimal price;
+    private int inventory;
+    private String description;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "category_id")
+    private Category category;
+
+    @OneToMany(cascade = CascadeType.ALL,mappedBy = "product",orphanRemoval = true)
+    private List<Image> images;
+
+}
