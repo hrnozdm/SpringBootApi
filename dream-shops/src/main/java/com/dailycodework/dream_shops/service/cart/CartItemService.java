c08c6c98616cb2c07808a7d571abbcc8d5ab8fdc
Harun Özdemir <80276787+hrnozdm@users.noreply.github.com>
2024-09-29 01:28:30 +0300

    CartService Update

--- dream-shops/src/main/java/com/dailycodework/dream_shops/service/cart/CartItemService.java
+++ dream-shops/src/main/java/com/dailycodework/dream_shops/service/cart/CartItemService.java
@@ -1,18 +1,84 @@
 package com.dailycodework.dream_shops.service.cart;
+import com.dailycodework.dream_shops.Model.Cart;
+import com.dailycodework.dream_shops.Model.CartItem;
+import com.dailycodework.dream_shops.Model.Product;
+import com.dailycodework.dream_shops.exception.ResourceNotFoundException;
+import com.dailycodework.dream_shops.repository.CartRepository;
+import com.dailycodework.dream_shops.service.product.IProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
+
+@Service
+@RequiredArgsConstructor
 public class CartItemService implements ICartItemService{
+
+    private final CartRepository cartRepository;
+    private final IProductService productService;
+    private final ICartService cartService;
+
     @Override
-    public void addItemToCart(Long cartId, Long productId, int quantity) {
-        
+    public void addItemToCart(Long cartId, Long productId, int quantity){
+        Cart cart = cartService.getCart(cartId);
+        Product product = productService.getProductById(productId);
+        CartItem cartItem = cart.getItems().stream()
+                .filter(item -> item.getProduct().getId().equals(product.getId()))
+                .findFirst()
+                .orElse(null);
+
+        if (cartItem != null) {
+            cartItem.setQuantity(cartItem.getQuantity() + quantity);
+            cartItem.setTotalPrice();
+
+        } else {
+            CartItem newCartItem = new CartItem();
+            newCartItem.setCart(cart);
+            newCartItem.setProduct(product);
+            newCartItem.setQuantity(quantity);
+            newCartItem.setUnitPrice(product.getPrice());
+            newCartItem.setTotalPrice();
+            cart.addItem(newCartItem);
+        }
+
+        cartRepository.save(cart);
     }
+
 
     @Override
     public void removeItemFromCart(Long cartId, Long productId) {
-
+        Cart cart=cartService.getCart(cartId);
+        CartItem itemToRemove=getCartItem(cartId,productId);
+        cart.removeItem(itemToRemove);
+        cartRepository.save(cart);
     }
 
     @Override
     public void updateItemQuantity(Long cartId, Long productId, int quantity) {
+        Cart cart = cartService.getCart(cartId);
+        cart.getItems()
+                .stream()
+                .filter(item -> item.getProduct().getId().equals(productId))
+                .findFirst()
+                .ifPresent(item -> {
+                    item.setQuantity(quantity);
+                    item.setUnitPrice(item.getProduct().getPrice());
+                    item.setTotalPrice();
+                });
+        BigDecimal totalAmount = cart.getItems()
+                .stream().map(CartItem ::getTotalPrice)
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
 
+        cart.setTotalAmount(totalAmount);
+        cartRepository.save(cart);
+    }
+
+    @Override
+    public CartItem getCartItem(Long cartId, Long productId) {
+        Cart cart = cartService.getCart(cartId);
+        return  cart.getItems()
+                .stream()
+                .filter(item -> item.getProduct().getId().equals(productId))
+                .findFirst().orElseThrow(() -> new ResourceNotFoundException("Item not found"));
     }
 }
